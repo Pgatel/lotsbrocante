79b52911257bd1ca7e8ab042591d7267f7000757
pascal.gatellier@temploux.be <pascal.gatellier@temploux.be>
2024-08-04 20:31:27 +0000

    Edited settings, LotsBrocante and UpdateLots

--- client_code/LotsBrocante/__init__.py
+++ client_code/LotsBrocante/__init__.py
@@ -10,18 +10,24 @@ class LotsBrocante(LotsBrocanteTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    zones = anvil.server.call("get_zone")
+    self.zone.items = zones
+    lots = anvil.server.call("get_lots", self.pair.checked, self.zone.selected_value)
+    self.lot.items = lots
 
   def lot_change(self, **event_args):
     """Search in datatable this number"""
-    pass
+    print(self.lot.selected_value)
 
   def populate_click(self, **event_args):
     anvil.server.call("update_lots_brocante")
 
   def zone_change(self, **event_args):
     """This method is called when an item is selected"""
-    pass
+    lots = anvil.server.call("get_lots", self.pair.checked, self.zone.selected_value)
+    self.lot.items = lots
 
   def pair_change(self, **event_args):
     """This method is called when this checkbox is checked or unchecked"""
-    pass
+    lots = anvil.server.call("get_lots", self.pair.checked, self.zone.selected_value)
+    self.lot.items = lots
--- server_code/UpdateLots.py
+++ server_code/UpdateLots.py
@@ -7,25 +7,49 @@ import anvil.server
 import pandas as pd
 from pandas import DataFrame
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-for zone in app_tables.zones:
-  print(Zone)
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
+@anvil.server.callable
+def get_zone():
+  l_zone = [row['zone'] for row in app_tables.zones.search()]
+  print(l_zone)
+  return l_zone
+
+@anvil.server.callable
+def get_lots(pair, zone):
+  print(f'Pair:{pair}; zone:{zone}')
+  filtered = app_tables.lotsbrocante.search(Zone=zone)
+  print(filtered)
+  return ['A024', 'A026']
+
 @anvil.server.callable
 def update_lots_brocante():
   path_lot = data_files['Liste des Lots réservés.xlsx']
   print(f"Updating the data table 'LotsBrocante' from {path_lot}")
   lots = pd.read_excel(path_lot)
+
+  app_tables.lotsbrocante.delete_all_rows()
+  for t_lot in lots.iterrows():
+    lot = t_lot[1]
+    if pd.isna(lot['Prénom']):
+      lot['Prénom'] = ''
+    if pd.isna(lot['Numero']):
+      lot['Numero'] = ''
+    if pd.isna(lot['Rue']):
+      lot['Rue'] = ''
+    if pd.isna(lot['Localité']):
+      lot['Localité'] = ''
+    if pd.isna(lot['Code Postal']):
+      lot['Code Postal'] = ''
+    print(lot)
+    app_tables.lotsbrocante.add_row(Nom=lot['Nom'], Prénom=lot['Prénom'], NumeroLot=lot['No Lot'],
+                                   Rue=lot['Rue'], Numero=lot['Numero'], Code_Postal=lot['Code Postal'],
+                                   Localité=lot['Localité'], Zone=lot['Zone'], Facade=lot['Facade'],
+                                   Profondeur=lot['Profondeur'], Surface=lot['Surface'])
+  
   zones = lots[['Zone']].groupby('Zone').first().index
   df_zones = DataFrame(zones)
   zones_pair = df_zones[df_zones['Zone'].str.contains(' pair')].values.tolist()
   s_zone_pair = [str(z[0])[:2].strip() for z in zones_pair]
+  
   app_tables.zones.delete_all_rows()
   for zone in s_zone_pair:
-    print(zone)
-    app_tables.zones.add_row(Zone=zone)
+    app_tables.zones.add_row(zone=zone)
