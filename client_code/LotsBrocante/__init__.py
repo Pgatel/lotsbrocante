e37d34f70b0fb4ec1a9e5b21a74fb13343acf5ed
pascal.gatellier@temploux.be <pascal.gatellier@temploux.be>
2024-08-04 18:46:54 +0000

    Edited settings, LotsBrocante and UpdateLots

--- client_code/LotsBrocante/__init__.py
+++ client_code/LotsBrocante/__init__.py
@@ -11,9 +11,17 @@ class LotsBrocante(LotsBrocanteTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-  def Lot_change(self, **event_args):
+  def lot_change(self, **event_args):
     """Search in datatable this number"""
     pass
 
-  def Populate_click(self, **event_args):
+  def populate_click(self, **event_args):
     anvil.server.call("update_lots_brocante")
+
+  def zone_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
+
+  def pair_change(self, **event_args):
+    """This method is called when this checkbox is checked or unchecked"""
+    pass
--- server_code/UpdateLots.py
+++ server_code/UpdateLots.py
@@ -5,17 +5,27 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 import pandas as pd
+from pandas import DataFrame
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
+for zone in app_tables.zones:
+  print(Zone)
 # To allow anvil.server.call() to call functions here, we mark
 # them with @anvil.server.callable.
 # Here is an example - you can replace it with your own:
 #
 @anvil.server.callable
 def update_lots_brocante():
-   path_lot = 'Liste des Lots réservés.xlsx'
-   print(f"Updating the data table 'LotsBrocante' from {path_lot}")
-   df_list = pd.read_excel(data_files[path_lot])
-   print(df_list)
+  path_lot = data_files['Liste des Lots réservés.xlsx']
+  print(f"Updating the data table 'LotsBrocante' from {path_lot}")
+  lots = pd.read_excel(path_lot)
+  zones = lots[['Zone']].groupby('Zone').first().index
+  df_zones = DataFrame(zones)
+  zones_pair = df_zones[df_zones['Zone'].str.contains(' pair')].values.tolist()
+  s_zone_pair = [str(z[0])[:2].strip() for z in zones_pair]
+  app_tables.zones.delete_all_rows()
+  for zone in s_zone_pair:
+    print(zone)
+    app_tables.zones.add_row(Zone=zone)
