728a5dccd01d0c8ca08fc01e4fb7833053fa43fd
pascal.gatellier@temploux.be <pascal.gatellier@temploux.be>
2024-08-04 16:27:01 +0000

    Edited LotsBrocante

--- client_code/LotsBrocante/__init__.py
+++ client_code/LotsBrocante/__init__.py
@@ -3,15 +3,22 @@ from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+import pandas as pd
 
 class LotsBrocante(LotsBrocanteTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+  def read_lots(self, path_lots):
+    df_list = pd.read_excel(path_lots)
+    return df_list
 
   def Lot_change(self, **event_args):
     """Search in datatable this number"""
     pass
+
+  def Populate_click(self, **event_args):
+    path_lot = r'C:\Brocante\Liste des Lots réservés.xlsx'
+    lots = self.read_lots(path_lot)
+    print(lots)
